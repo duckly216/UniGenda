a468eb9d501954a110beededa29521c0b555e003
Bradley Cederholm <106129785+Hqllow@users.noreply.github.com>
2026-03-09 12:13:42 -0400

    Clean up home title page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,34 +2,35 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navigate, Link } from 'react-router-dom';
 import Login from './components/Login';
+import "./styles/Home.css";
 // import Dashboard from './components/Dashboard';
 
 function App() {
-    return (
-      <Router>
-        <div className="App">
-          <Routes>
-            {/*Automatically routes to login page*/}
-            <Route
-              path="/"
-              element={
-                <div>
-                  <h1>Welcome to UniGenda</h1>
-                  <Link to="/login">
-                    <button>Go to Login</button>
-                  </Link>
-                </div>
-              }
-            />
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {/*Automatically routes to login page*/}
+          <Route
+            path="/"
+            element={
+              <div className="home-container">
+                <h1>UniGenda</h1>
+                <Link to="/login">
+                  <button>Go to Login</button>
+                </Link>
+              </div>
+            }
+          />
 
-            {/*When URL is /login, render the logic component*/}
-            <Route path="/login" element={<Login />} />
+          {/*When URL is /login, render the logic component*/}
+          <Route path="/login" element={<Login />} />
 
-            {/*When URL is /dashboard, render the logic component*/}
-            {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-          </Routes>
-        </div>
-      </Router>
-    );    
+          {/*When URL is /dashboard, render the logic component*/}
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 export default App;
